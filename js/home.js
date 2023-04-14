ccdf2c5c9048873d154059173a38121bc7c357b0
Kopatychub <safiullinbulat6@gmail.com>
2023-04-14 17:46:08 +0300

    апгрейд слайдер

--- js/home.js
+++ js/home.js
@@ -1,49 +1,58 @@
-// function create_imgs(){
-//     const spis = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple']
-
-//     for (let i = 0; i < spis.length; i++) {
-//         const x = spis[i];
-//         var box = document.createElement('div')
-//         box.className = 'slider_cont'
-//         box.innerHTML = '<img>'+x+'</img>'
-//         document.getElementById('slider').append(box)
-//     }
-// }
-// create_imgs()
-
 let slider_cfg = [
-"https://mobimg.b-cdn.net/v3/fetch/74/74739e1770f31cdbfdde99cc0b2925d3.jpeg?w=1470&r=0.5625",
-"https://w-dog.ru/wallpapers/10/18/426720569630139/solnechnyj-vodopad-les-nebo-solnce-voda.jpg",
-"https://i.pinimg.com/originals/76/cd/79/76cd79a8da202d46c88fc728e30e690f.jpg",
-"https://klike.net/uploads/posts/2022-09/1662635483_g-8.jpg"
+    "https://mobimg.b-cdn.net/v3/fetch/74/74739e1770f31cdbfdde99cc0b2925d3.jpeg?w=1470&r=0.5625",
+    "https://w-dog.ru/wallpapers/10/18/426720569630139/solnechnyj-vodopad-les-nebo-solnce-voda.jpg",
+    "https://i.pinimg.com/originals/76/cd/79/76cd79a8da202d46c88fc728e30e690f.jpg",
+    "https://klike.net/uploads/posts/2022-09/1662635483_g-8.jpg",
+    'img/fire.jpg',
+    'img/fire3.png'
 ]
 let num = 0
+const slider_image = document.getElementById('slider_image');
+const img_style = slider_image.style
 function slide(side) {
-    let slider_image = document.getElementById('slider_image');
     document.getElementById('slider').scrollIntoView({
         behavior: 'smooth',
         block: 'start'
     })
-    slider_image.style.opacity = '0'
-    setTimeout(function() {
-        if (side == 'right') {
-            if (num < slider_cfg.length - 1) {
-                num++
-            } else {
-                num = 0
-            }
-            slider_image.src = slider_cfg[num]
-        } else if (side == 'left') {
+    if (side == 'right') {
+        img_style.opacity = '0'
+        img_style.transform = 'translate(-500px, 0)'
+        img_style.height = '330px'
+        setTimeout(() => {
+            img_style.transition = 0
+            img_style.transform = 'translate(500px, 0)'
             if (num != 0) {
                 num--
             } else {
                 num = slider_cfg.length - 1
             }
             slider_image.src = slider_cfg[num]
-        }
-        slider_image.style.opacity = '1'
-    },500)
+        }, 250);
+        setTimeout(() => {
+            img_style.transition = '.25s'
+            img_style.transform = 'translate(0, 0)'
+            img_style.opacity = 1
+            img_style.height = '400px'
+        }, 500);
+    } else if (side == 'left') {
+        img_style.opacity = '0'
+        img_style.transform = 'translate(500px, 0)'
+        img_style.height = '330px'
+        setTimeout(() => {
+            img_style.transition = 0
+            img_style.transform = 'translate(-500px, 0)'
+            if (num != 0) {
+                num--
+            } else {
+                num = slider_cfg.length - 1
+            }
+            slider_image.src = slider_cfg[num]
+        }, 250);
+        setTimeout(() => {
+            img_style.transition = '.25s'
+            img_style.transform = 'translate(0, 0)'
+            img_style.opacity = 1
+            img_style.height = '400px'
+        }, 500);
+    }
 }
-
-// varibles
-
